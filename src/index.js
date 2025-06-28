401790e78ecd45792d1f1c656bcbf663e1d03dcb
Andres <andresngomez@live.com>
2025-06-28 15:37:23 -0600

    no hice nada, cambie dos requires de orden en el index

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ app.setup({
     enableHttpStream: true,
 });
 
-require('./functions/dbModifiers/addSensor');
 require('./functions/dbModifiers/postStatus');
+require('./functions/dbModifiers/addSensor');
 require('./functions/dbModifiers/addClient');
 require('./functions/dbModifiers/addParking');
 require('./functions/dbModifiers/addLevel');
